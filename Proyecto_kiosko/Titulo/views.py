ee6e8a57a348de56b0b365fad8c4140b6a1e3940
bardokx <bardok311@gmail.com>
2022-11-18 19:56:19 -0300

    Me pegue un cagazo pero tengo un backup
    ignorenlo todo ta bn

--- Proyecto_kiosko/Titulo/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-
-def home(request):
-    return render(request, 'home.html')
-
-def start_session(request):
-    return render(request, 'login.html')

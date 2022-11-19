6a1ebf5b319076a77853036b0ea98c5f64cfe779
bardokx <bardok311@gmail.com>
2022-11-19 18:55:53 -0300

    Cambios post ayudantia

--- Titulo/urls.py
+++ Titulo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.homepage),
     path("login/", views.start_session),
+    path("virtual_row/", views.waiting_row),
+    path("terms_and_conditions/", views.terms),
+    path("payment/", views.pay),
 ]
--- Titulo/views.py
+++ Titulo/views.py
@@ -2,8 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
-def home(request):
+def homepage(request):
     return render(request, 'home.html')
 
 def start_session(request):
     return render(request, 'login.html')
+
+def waiting_row(request):
+    return render(request, "row.html")
+
+def terms(request):
+    return render(request, "terms_conditions.html")
+
+def pay(request):
+    return render(request, "pay.html")

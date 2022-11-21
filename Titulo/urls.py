4a7c8399751d4bafaae6d602cb554161411802d4
CarlosRabu <carloslbsg@gmail.com>
2022-11-21 00:02:53 -0300

    omit

--- Titulo/urls.py
+++ Titulo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage),
-    path("login/", views.start_session),
+    path("", views.start_session),
+    path("home/", views.homepage),
     path("virtual_row/", views.waiting_row),
     path("terms_and_conditions/", views.terms),
     path("payment/", views.pay),
